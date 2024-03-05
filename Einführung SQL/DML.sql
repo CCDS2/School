c9636b3af5897a221b98b3a0da29e85734cdaab7
CDS2 <148057750+CCDS2@users.noreply.github.com>
2024-03-05 11:27:30 +0100

    Update DML.sql

--- Einführung SQL/DML.sql
+++ Einführung SQL/DML.sql
@@ -1,16 +1,3 @@
-
-ALTER TABLE employees
-ADD COLUMN department VARCHAR(50);
-
-ALTER TABLE employees
-MODIFY COLUMN salary FLOAT(10,2);
-
-ALTER TABLE employees
-CHANGE COLUMN department department_id INT;
-
-ALTER TABLE employees
-DROP COLUMN email;
-
 -- Tabellenstruktur für Tabelle bluray_sammlung
 DROP TABLE IF EXISTS bluray_sammlung;
 CREATE TABLE bluray_sammlung (
